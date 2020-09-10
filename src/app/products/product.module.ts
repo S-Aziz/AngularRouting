cf7d281e11f430160c2d46c352c7d463964aa459
azizsalhi.ionic <azizsalhi.ionic@gmail.com>
2020-09-10 11:27:39 +0100

    CanDeactivate Guard

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -8,7 +8,7 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
 import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 import { ProductResolver } from './product-resolver.service';
-
+import { ProductEditGuard } from './product-edit/product-edit.guard';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
@@ -31,6 +31,7 @@ import { SharedModule } from '../shared/shared.module';
           {
             path: ':id/edit',
             component: ProductEditComponent,
+            canDeactivate: [ProductEditGuard],
             resolve: { resolvedData: ProductResolver },
             children: [
               { path: '', redirectTo: 'info', pathMatch: 'full' },
